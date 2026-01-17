e5e5ce25df750fda3e7888ec6db9c19606818b12
George Dietrich <dev@dietrich.pub>
2026-01-17 15:20:38 -0500

    Cleanup

--- src/mapping/class_factory_interface.cr
+++ src/mapping/class_factory_interface.cr
@@ -1,6 +1,3 @@
 module Athena::ORM::Mapping::ClassFactoryInterface
   abstract def metadata(for entity_class : AORM::Entity.class) : AORM::Mapping::ClassInterface
-
-  # Should *entity_class* have its metadata loaded?
-  # abstract def transient?(entity_class : AORM::Entity.class) : Bool
 end
--- src/mapping/value.cr
+++ src/mapping/value.cr
@@ -1,4 +1,6 @@
 module Athena::ORM::Mapping
+  # Container type for holding arbitrary values for a given column
+
   abstract struct Value; end
 
   record ColumnValue(T) < Athena::ORM::Mapping::Value, name : String, value : T
